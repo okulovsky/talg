3dd686109248dbd9e90010c76d378e56f0606d8e
okulovsky <yuri.okulovsky@gmail.com>
2015-12-23 15:42:01 +0500

    + BF  Generator

---
+++ Programs/BFGenerator/Functions.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFGenerator
+{
+	public abstract class BooleanFunction
+	{
+		public abstract bool Evaluate(bool[] arguments);
+		public abstract string Tex();
+		public abstract string ToLetter();
+		public abstract IEnumerable<BooleanFunction> Subtree { get;  }
+	}
+
+	public class Variable : BooleanFunction
+	{
+		public int Number;
+		public const string Names = "xyzuvw";
+		public override string ToString()
+		{
+			return Names[Number].ToString();
+		}
+
+		public override string Tex()
+		{
+			return ToString();
+		}
+
+		public override string ToLetter()
+		{
+			 return ToString();
+		}
+
+
+		public override bool Evaluate(bool[] arguments)
+		{
+			return arguments[Number];
+		}
+		public override IEnumerable<BooleanFunction> Subtree
+		{
+			get { yield return this; }
+		}
+	}
+
+	public abstract class Operation : BooleanFunction 
+	{
+		public string Letter { get; set; }
+		public override string ToLetter()
+		{
+			return Letter;
+		}
+		public abstract string LetterTex();
+	}
+
+	public class Negation : Operation
+	{
+		public BooleanFunction InnerFunction;
+
+		public override string ToString()
+		{
+			return "(!" + InnerFunction.ToString()+")";
+		}
+		public override string Tex()
+		{
+			return "(\\neg " + InnerFunction.Tex() + ")";
+		}
+		public override string LetterTex()
+		{
+			return "\\neg " + InnerFunction.ToLetter();
+		}
+
+		public override bool Evaluate(bool[] arguments)
+		{
+			return !InnerFunction.Evaluate(arguments);
+		}
+
+
+		public override IEnumerable<BooleanFunction> Subtree
+		{
+			get
+			{
+				yield return this;
+				foreach (var e in InnerFunction.Subtree)
+					yield return e;
+			}
+		}
+	}
+
+	public class BinaryFunction : Operation
+	{
+		public string Sign;
+		public string TexSign;
+		public Func<bool, bool, bool> Func;
+		public BooleanFunction Left;
+		public BooleanFunction Right;
+
+		public override string Tex()
+		{
+			return "(" + Left.Tex() + TexSign +" "+ Right.Tex() + ")";
+		}
+
+		public override string ToString()
+		{
+			return "(" + Left.ToString() + Sign + Right.ToString() + ")";
+		}
+
+		public override bool Evaluate(bool[] arguments)
+		{
+			return Func(Left.Evaluate(arguments), Right.Evaluate(arguments));
+		}
+
+		public BinaryFunction(string sign, string texsign, Func<bool,bool,bool> func)
+		{
+			this.TexSign = texsign;
+			this.Sign = sign;
+			this.Func = func;
+		}
+
+		public static IEnumerable<Func<BinaryFunction>> All
+		{
+			get
+			{
+				yield return () => new BinaryFunction("||","\\wedge", (a, b) => a && b);
+				yield return () => new BinaryFunction("&&","\\vee", (a, b) => a || b);
+				yield return () => new BinaryFunction("->","\\rightarrow", (a, b) => !a || b);
+				yield return () => new BinaryFunction("+","\\oplus", (a, b) => (a&&!b)||(!a&&b));
+			}
+		}
+
+		public override string LetterTex()
+		{
+			return Left.ToLetter() + TexSign + " " + Right.ToLetter();
+		}
+
+		public override IEnumerable<BooleanFunction> Subtree
+		{
+			get
+			{
+				yield return this;
+
+				foreach (var e in Right.Subtree) yield return e;
+				foreach (var e in Left.Subtree) yield return e;
+			}
+		}
+
+	}
+}
---
+++ Programs/BFGenerator/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFGenerator
+{
+	public class Program
+	{
+		static Random rnd = new Random(1);
+		static Func<BinaryFunction>[] funcs = BinaryFunction.All.ToArray();
+		const int negationProbability = 20;
+
+		static BooleanFunction CreateNegation(BooleanFunction f)
+		{
+			if (rnd.Next(100) < negationProbability)
+				return new Negation { InnerFunction = f };
+			return f;
+		}
+
+		static BooleanFunction CreateUnary(bool[] usedVariables)
+		{
+			int variable=-1;
+			var unused = Enumerable.Range(0, usedVariables.Length).Where(z => !usedVariables[z]).ToList();
+			if (unused.Count != 0)
+				variable = unused[rnd.Next(unused.Count)];
+			else
+				variable = rnd.Next(usedVariables.Length);
+			var f = new Variable { Number = variable };
+			usedVariables[variable] = true;
+			return CreateNegation(f);
+		}
+
+		static BooleanFunction CreateBinary(bool[] usedVariables, int functionsCount)
+		{
+			if (functionsCount == 0) return CreateUnary(usedVariables);
+			var leftCount = rnd.Next(0, functionsCount - 1);
+			var rightCount = functionsCount - 1 - leftCount;
+			var function = funcs[rnd.Next(funcs.Length)]();
+			function.Left = CreateBinary(usedVariables, leftCount);
+			function.Right = CreateBinary(usedVariables, rightCount);
+			return CreateNegation(function);
+		}
+
+	
+		static string Cell(bool value)
+		{
+			return (value ? "1" : "0") + "&";
+		}
+
+		static string MakeText(int number, BooleanFunction f, int varCount, bool withAnswer)
+		{
+			StringBuilder builder = new StringBuilder();
+
+			builder.Append("Case \\#" + number);
+			
+			var operations = f.Subtree.OfType<Operation>().Reverse().ToArray();
+			for (int i = 0; i < operations.Length; i++)
+				operations[i].Letter = ((char)('A' + i)).ToString();
+
+			string arrayTail = "";
+			for (int i = 0; i < varCount; i++)
+				arrayTail += "c";
+			arrayTail += "||";
+			for (int i = 0; i < operations.Length; i++)
+			{
+				if (i != 0 && i % 3 == 0)
+					arrayTail += "|"; 
+				arrayTail += "c";
+			}
+			arrayTail += "c";
+			
+
+			builder.Append("$$\n"+f.Tex() + "$$\n\n");
+
+			builder.Append("\\begin{tabular}{" + arrayTail + "}\n");
+			for (int i = 0; i < varCount; i++)
+				builder.Append(Variable.Names[i] + "&");
+			foreach (var e in operations)
+				builder.Append("\\rotatebox{90}{$" + e.Letter + "=" + e.LetterTex() + "$}&");
+			builder.Append("\\\\\n\\hline\n");
+
+			var values = new bool[varCount];
+			do
+			{
+				for (int i = 0; i < varCount; i++)
+					builder.Append(Cell(values[i]));
+				foreach (var e in f.Subtree.OfType<Operation>().Reverse())
+					builder.Append(withAnswer?Cell(e.Evaluate(values)):"&");
+				builder.Append("\\\\\n\\hline\n");
+			} while (Next(values));
+			builder.Append("\\end{tabular}\n\n");
+			builder.Append("\\pagebreak\n\n");
+			return builder.ToString();
+		}
+
+		static string Exercise(int number)
+		{
+			int varCount = 5;
+			int funcCount = 9;
+			
+			BooleanFunction f = null;
+			while (true)
+			{
+				
+				f = CreateBinary(new bool[varCount], funcCount);
+				var factVarCount = f.Subtree.OfType<Variable>().Select(z => z.Number).Distinct().Count();
+				if (factVarCount == varCount) break;
+			}
+			return MakeText(number,f, varCount,false)+MakeText(number,f, varCount,true);
+		}
+
+		public static bool Next(bool[] values)
+		{
+			for (int i = values.Length - 1; i >= 0; i--)
+				if (!values[i])
+				{
+					values[i] = true;
+					return true;
+				}
+				else
+					values[i] = false;
+			return false;
+		}
+
+		public static void Main()
+		{
+			var s = Exercise(1);
+			Console.WriteLine(s);
+			File.WriteAllText("..\\..\\..\\BFGeneratorLatex\\output.tex",s);
+
+		}
+
+	}
+}
